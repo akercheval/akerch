bc61ba872e7a5c13e2110a1cf657734c5ceadad1
Adam Kercheval <arkercheval@gmail.com>
2017-06-18 22:15:20 -0400

    firts commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $("#Lato").click(function() {
+        $('#Header').css("font-family", "Lato");
+    });
+    $("#Verdana").click(function() {
+        $('#Header').css("font-family", "Verdana");
+    });
+    $("#Impact").click(function() {
+        $('#Header').css("font-family", "Impact");
+    });
+    $("#Comic").click(function() {
+        $('#Header').css("font-family", "Comic Sans MS");
+    });
+});
