408778d4e27771e4c8b0ac5d69144612cbba65eb
Kercheval <akerchev@amazon.com>
2017-06-20 17:33:16 -0400

    more updates!

--- js/hello.js
+++ js/hello.js
@@ -1,7 +1,16 @@
 $(document).ready(function() {
-    $("#Header").css("font-family", localStorage.getItem("head"));
+    $(".Header").css("font-family", localStorage.getItem("head"));
     if (localStorage.getItem("weight") != null) {
-        $("#Header").css("font-weight", localStorage.getItem("weight"));
+        $(".Header").css("font-weight", localStorage.getItem("weight"));
     }
-    $("#Body").css("font-family", localStorage.getItem("body"));
+    $(".Body").css("font-family", localStorage.getItem("body"));
+    var dt = new Date($.now());
+    hour = dt.getHours();
+    if ((0 <= hour && hour < 5) || (19 <= hour && hour <= 24)) {
+        $("#Welcome").html("Good evening!");
+    } else if (5 <= hour && hour < 12) {
+        $("#Welcome").html("Good morning!");
+    } else {
+        $("#Welcome").html("Good afternoon!");
+    }
 });
