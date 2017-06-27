bf06583322620a9188f6e576574e7c7ae277249d
Kercheval <akerchev@amazon.com>
2017-06-27 15:13:46 -0400

    updated resume

--- js/morf.js
+++ js/morf.js
@@ -1,7 +1,26 @@
 $(document).ready(function() {
     $("#button").on("click", function() {
         word = $("#word").val();
-        $.ajax({
+        var xhr = new XMLHttpRequest();
+        xhr.open("GET", "https://od-api.oxforddictionaries.com:443/api/v1/entries/es/palabra", true);
+
+        xhr.setRequestHeader("Accept", "application/json");
+
+        xhr.setRequestHeader("app_id", "5eb6d713");
+
+        xhr.setRequestHeader("app_key", "330c94ccd9813cfb8e40ecf9196992f8");
+
+		//xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		//var requeststr = "username=STgg85uu&lat=" + myLat + "&lng=" +myLng;
+		//var what_am_i;
+		xhr.send();
+        alert('here');
+        alert(xhr.responseText);
+
+        var response = JSON.parse(xhr.responseText);
+
+        alert(response);
+        /*$.ajax({
             url: "https://od-api.oxforddictionaries.com:443/api/v1/entries/es/" + word,
             type: "GET",
             beforeSend: function (xhr) {
@@ -11,6 +30,6 @@ $(document).ready(function() {
             },
             error: function(xhr, ajaxOptions, thrownError) { alert(xhr.responseText); },
             success: function() { alert('Success!' + word); }
-          });
+        });*/
     });
 });
