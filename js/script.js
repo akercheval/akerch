9042d338cdd0a12d70ff74e06d3ee5a601a73260
Adam Kercheval <arkercheval@gmail.com>
2018-06-23 16:25:35 -0300

    clock overhaul

--- js/script.js
+++ js/script.js
@@ -3,32 +3,44 @@ $(document).keypress(function(event){
   var keycode = (event.keyCode ? event.keyCode : event.which);
   switch(keycode) {
     case 50: // 2
-      $("#Helvetica").css("font-family", "MyGaramond");
+      $("#hourhand").css("font-family", "MyGaramond");
       break;
     case 51: // 3
-      $("#Helvetica").css("font-family", "Roboto");
+      $("#hourhand").css("font-family", "Roboto");
       break;
     case 52: // 4
-      $("#Helvetica").css("font-family", "Inconsolata-dz");
+      $("#hourhand").css("font-family", "Inconsolata-dz");
       break;
     case 53: // 5
-      $("#Helvetica").css("font-family", "Proxima");
+      $("#hourhand").css("font-family", "Proxima");
       break;
     case 54: // 6
-      $("#Helvetica").css("font-family", "Georgia");
+      $("#hourhand").css("font-family", "Georgia");
       break;
     case 55: // 7
-      $("#Helvetica").css("font-family", "Iosevka");
+      $("#hourhand").css("font-family", "Iosevka");
       break;
     case 56: // 8
-      $("#Helvetica").css("font-family", "Mononoki");
+      $("#hourhand").css("font-family", "Mononoki");
       break;
     default: // 1, or other
-      $("#Helvetica").css("font-family", "HelveticaBold");
+      $("#hourhand").css("font-family", "Helvetica");
   }
 });
-$("#a").hover(function(){
+
+$("#clock").hover(function(){
   $(document.body).append("<p class='littleHint'>1, 2, 3...</p>")
 }, function(){
   $(".littleHint").remove()
 });
+
+updateTime();
+function updateTime() { // Update the SVG clock
+        var now = new Date();
+        var min = now.getMinutes();
+        var hour = (now.getHours() % 12) + min/60;
+        var hourangle = (hour * 30) - 90;
+        var hourhand = document.getElementById("hourhand");
+        hourhand.setAttribute("transform", "rotate(" + hourangle + " 50 50)");
+        setTimeout(updateTime, 1000);
+}
